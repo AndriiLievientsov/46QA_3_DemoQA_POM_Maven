4002a7d794744c11ea3c38b3d93dab285a1f7ec1
anli <4uddik2000@gmail.com>
2025-07-24 19:38:35 +0300

    Class 23 part 1

--- 46QA_3_DemoQA_POM_Maven/src/main/java/demoqa/pages/HomePage.java
+++ 46QA_3_DemoQA_POM_Maven/src/main/java/demoqa/pages/HomePage.java
@@ -2,6 +2,10 @@ package demoqa.pages;
 
 import demoqa.core.BasePage;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.nio.file.WatchEvent;
 
 public class HomePage extends BasePage {
 
@@ -9,4 +13,11 @@ public class HomePage extends BasePage {
         super(driver);
     }
 
+    @FindBy(css = "(//div[@class='card mt-4 top-card'])[6]")
+    WebElement bookStore;
+
+    public SidePage getBookStore() {
+        click(bookStore);
+        return  new SidePage(driver);
+    }
 }
--- 46QA_3_DemoQA_POM_Maven/src/main/java/demoqa/pages/SidePage.java
+++ 46QA_3_DemoQA_POM_Maven/src/main/java/demoqa/pages/SidePage.java
@@ -2,11 +2,21 @@ package demoqa.pages;
 
 import demoqa.core.BasePage;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class SidePage extends BasePage {
 
     public SidePage(WebDriver driver) {
+
         super(driver);
     }
 
+    @FindBy(xpath = "//span[normalize-space()='Login']")
+    WebElement login;
+
+    public LoginPage selectLogin() {
+        click(login);
+        return new LoginPage(driver);
+    }
 }
